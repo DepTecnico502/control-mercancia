3f8c051a905646cbf8425f417650a8fb01f8f751
DepTecnico502 <deptecnico502@gmail.com>
2024-03-01 08:06:13 -0600

    Consumiendo api creada desde Google Sheets y modulo de articulos en bodega

--- app/Http/Controllers/ArticuloBodegaController.php
+++ app/Http/Controllers/ArticuloBodegaController.php
@@ -2,12 +2,78 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Partida;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ArticuloBodegaController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('articulosBodega.index');
+        $response = Http::get('https://script.google.com/macros/s/AKfycbxLBXyoD05aRtMHCX9mQEQvGsV41E17Ej0qcEiDnLod5nAwPRmk9-D9eqjttNf1_se8MQ/exec', [
+            'sheet' => 'articulos-transito',
+        ]);
+    
+        $articulosTransito = $response->json();
+    
+        // Filtrar los resultados si hay un valor en el campo de búsqueda
+        if ($request->has('buscar')) {
+            $filtro = $request->input('buscar');
+            $articulosTransito = array_filter($articulosTransito, function ($articulo) use ($filtro) {
+                return 
+                    stripos($articulo['nombre_proveedor'], $filtro) !== false
+                    || stripos($articulo['numero_documento'], $filtro) !== false
+                    || stripos($articulo['numero_articulo'], $filtro) !== false
+                    || stripos($articulo['descripcion_articulo'], $filtro) !== false;
+            });
+        }
+
+        $noPartidas = Partida::pluck('no_partida')->toArray();
+        $numerosPartidaDeseados = $noPartidas;
+        $articulosTransito = array_filter($articulosTransito, function ($articulo) use ($numerosPartidaDeseados) {
+            return in_array($articulo['numero_documento'], $numerosPartidaDeseados);
+        });
+
+        // Asociar el estado a cada artículo
+        foreach ($articulosTransito as &$articulo) {
+            $articulo['estado'] = $this->determinarEstado($articulo);
+        }
+
+        return view('articulosBodega.index', [
+            'articulosTransito' => $articulosTransito,
+        ]);
     }
+
+    private function determinarEstado($articulo)
+    {
+        // Buscar la partida correspondiente al número de documento
+        $partida = Partida::where('no_partida', $articulo['numero_documento'])->first();
+        
+        // dd($articulo['numero_documento']);
+        // Verificar el estado basado en las fechas en la partida
+        if (!is_null($partida->fecha_valorizado) && is_null($partida->fecha_liberado) && is_null($partida->fecha_colocado)) {
+            return 'Valorizado';
+        } elseif (!is_null($partida->fecha_valorizado) && !is_null($partida->fecha_liberado) && is_null($partida->fecha_colocado)) {
+            return 'Liberado';
+        } elseif (!is_null($partida->fecha_valorizado) && !is_null($partida->fecha_liberado) && !is_null($partida->fecha_colocado)) {
+            return 'Colocado';
+        } else {
+            return 'Recibido';
+        }
+    }
+
+    // public function index()
+    // {
+    //     $response = Http::get('https://script.google.com/macros/s/AKfycbxLBXyoD05aRtMHCX9mQEQvGsV41E17Ej0qcEiDnLod5nAwPRmk9-D9eqjttNf1_se8MQ/exec', [
+    //         'sheet' => 'articulos-transito',
+    //     ]);
+
+    //     $articulosTransito = $response->json();
+
+    //     return view('articulosBodega.index', [
+    //         'articulosTransito' => $articulosTransito
+    //     ]);
+    // }
 }
--- resources/views/articulosBodega/index.blade.php
+++ resources/views/articulosBodega/index.blade.php
@@ -3,5 +3,74 @@
 @section('titulo', 'Artículos en bodega')
     
 @section('contenido')
-    
+<form action="{{ route('articulos.bodega.index') }}" method="GET">
+    <div class="md:grid md:grid-cols-3 gap-5">
+        <div class="mb-5">
+            <label 
+                class="block mb-1 text-sm text-gray-500 uppercase font-bold "
+                for="termino">Término de Búsqueda
+            </label>
+            <div class="relative">
+                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                    </svg>
+                </div>
+                <input 
+                    type="text" 
+                    name="buscar"
+                    id="buscar" 
+                    class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" 
+                    placeholder="Buscar Transporte, No Guía, Proveedor, Recibido, No Pedido."
+                    value="{{ request('buscar') }}"
+                />
+                <input 
+                    type="submit" 
+                    class="text-white absolute end-2.5 bottom-2.5 bg-green-500 hover:bg-green-600 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2" 
+                    value="Buscar"
+                />
+            </div>
+        </div>
+    </div>
+</form>
+{{-- <form action="{{ route('articulos.bodega.index') }}" method="get">
+    <input type="text" name="buscar" placeholder="Buscar por proveedor, código, etc." value="{{ request('buscar') }}">
+    <button type="submit">Buscar</button>
+</form> --}}
+<div class="overflow-x-auto">
+    <table class="table table-xs">
+        <thead>
+            <tr>
+                <th>No. partida</th> 
+                <th>proveedor</th> 
+                <th>codigo</th> 
+                <th>Descripcion</th> 
+                <th>cantidad</th>
+                <th>Estado</th>
+            </tr>
+        </thead> 
+        <tbody>
+            @foreach ($articulosTransito as $transito)
+                <tr>
+                    <td>{{ $transito['numero_documento'] }}</td> 
+                    <td>{{ $transito['nombre_proveedor'] }}</td> 
+                    <td>{{ $transito['numero_articulo'] }}</td> 
+                    <td>{{ $transito['descripcion_articulo'] }}</td> 
+                    <td>{{ $transito['cantidad'] }}</td>
+                    <td>
+                        @if($transito['estado'] === 'Recibido')
+                            <div class="badge badge-info text-white">Recibido</div>
+                        @elseif($transito['estado'] === 'Valorizado')
+                            <div class="badge badge-warning text-white">Valorizado</div>
+                        @elseif($transito['estado'] === 'Colocado')
+                            <div class="badge badge-primary text-white">Colocado</div>
+                        @elseif($transito['estado'] === 'Liberado')
+                            <div class="badge badge-success text-white">Liberado</div>
+                        @endif
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endsection
