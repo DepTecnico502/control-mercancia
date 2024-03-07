2d9ed8cd94c3b78f9cac397c6093e942abc99a99
DepTecnico502 <deptecnico502@gmail.com>
2024-03-07 11:29:30 -0600

    Cambio de zona horaria

--- app/Http/Controllers/CheckController.php
+++ app/Http/Controllers/CheckController.php
@@ -3,16 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\Partida;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class CheckController extends Controller
 {
     public function checkProductoNuevo(Partida $partida)
     {
+        // Obtiene la fecha y hora actual
+        $currentDateTime = Carbon::now();
+
+        // Formatea la fecha y hora como YYYY-MM-DD HH:mm:ss
+        $formattedDateTime = $currentDateTime->format('Y-m-d H:i:s');
+        
         // Verificar si la fecha_producto_nuevo es null
         if ($partida->fecha_producto_nuevo === null) {
             // Actualizar la fecha_producto_nuevo con la fecha actual
-            $partida->update(['fecha_producto_nuevo' => now()]);
+            $partida->update(['fecha_producto_nuevo' => $formattedDateTime]);
 
             // Redireccionar o devolver una respuesta
             return redirect()->back()->with('mensaje', 'Registro actualizado correctamente.');
@@ -24,8 +31,14 @@ class CheckController extends Controller
 
     public function checkValorizado(Partida $partida)
     {
+        // Obtiene la fecha y hora actual
+        $currentDateTime = Carbon::now();
+
+        // Formatea la fecha y hora como YYYY-MM-DD HH:mm:ss
+        $formattedDateTime = $currentDateTime->format('Y-m-d H:i:s');
+
         if ($partida->fecha_valorizado === null) {
-            $partida->update(['fecha_valorizado' => now()]);
+            $partida->update(['fecha_valorizado' => $formattedDateTime]);
 
             return redirect()->back()->with('mensaje', 'Registro actualizado correctamente.');
         } else {
@@ -35,8 +48,14 @@ class CheckController extends Controller
 
     public function checkLiberado(Partida $partida)
     {
+        // Obtiene la fecha y hora actual
+        $currentDateTime = Carbon::now();
+
+        // Formatea la fecha y hora como YYYY-MM-DD HH:mm:ss
+        $formattedDateTime = $currentDateTime->format('Y-m-d H:i:s');
+
         if ($partida->fecha_liberado === null) {
-            $partida->update(['fecha_liberado' => now()]);
+            $partida->update(['fecha_liberado' => $formattedDateTime]);
 
             return redirect()->back()->with('mensaje', 'Registro actualizado correctamente.');
         } else {
@@ -46,8 +65,14 @@ class CheckController extends Controller
 
     public function checkColocado(Partida $partida)
     {
+        // Obtiene la fecha y hora actual
+        $currentDateTime = Carbon::now();
+
+        // Formatea la fecha y hora como YYYY-MM-DD HH:mm:ss
+        $formattedDateTime = $currentDateTime->format('Y-m-d H:i:s');
+
         if ($partida->fecha_colocado === null) {
-            $partida->update(['fecha_colocado' => now()]);
+            $partida->update(['fecha_colocado' => $formattedDateTime]);
 
             return redirect()->back()->with('mensaje', 'Registro actualizado correctamente.');
         } else {
